743330160559244c8b08887f02ce805631df94f9
Vatsal <vatsalmori2005@gmail.com>
2026-05-27 11:26:25 +0530

    Added Backend APIs and interactive dashbord

---
+++ api/route_api.py
@@ -0,0 +1,82 @@
+import osmnx as ox
+
+from fastapi import APIRouter
+
+from algorithms.route_optimizer import RouteOptimizer
+
+from ml.traffic_model import TrafficModel
+from utils.helpers import route_length
+
+from ml.dataset_generator import generate_traffic_data
+
+traffic_model = TrafficModel()
+df = generate_traffic_data()
+traffic_model.train(df)
+router = APIRouter()
+
+
+# load graph once
+G = ox.graph_from_place("Ahmedabad, India", network_type="drive")
+
+optimizer = RouteOptimizer(G)
+
+
+@router.get("/route")
+def get_route(
+    origin_lat: float, origin_lon: float, destination_lat: float, destination_lon: float
+):
+
+    origin = optimizer.coordinates_to_node(origin_lat, origin_lon)
+
+    destination = optimizer.coordinates_to_node(destination_lat, destination_lon)
+
+    route = optimizer.shortest_route(origin, destination)
+
+    distance_route_coordinates = []
+
+    for node in route:
+        lat = G.nodes[node]["y"]
+
+        lon = G.nodes[node]["x"]
+
+        distance_route_coordinates.append([lat, lon])
+
+    distance_route = ox.shortest_path(G, origin, destination, weight="length")
+
+    traffic_route = ox.shortest_path(G, origin, destination, weight="traffic_weight")
+    traffic_route_coordinates = []
+
+    for node in traffic_route:
+        lat = G.nodes[node]["y"]
+
+        lon = G.nodes[node]["x"]
+
+        traffic_route_coordinates.append([lat, lon])
+    distance_length = route_length(G, distance_route)
+
+    traffic_length = route_length(G, traffic_route)
+    eta_distance = traffic_model.predict_eta(
+        distance=distance_length / 1000, hour=18, traffic_level=7, weather=0
+    )
+    eta_traffic = traffic_model.predict_eta(
+        distance=traffic_length / 1000, hour=18, traffic_level=3, weather=0
+    )
+
+    return {
+        # shortest-distance route
+        "shortest_route": {
+            "distance_meters": distance_length,
+            "eta_minutes": eta_distance,
+            "traffic_level": "Heavy",
+            "route_coordinates": distance_route_coordinates,
+            "nodes": len(distance_route),
+        },
+        # traffic-aware route
+        "traffic_aware_route": {
+            "distance_meters": traffic_length,
+            "eta_minutes": eta_traffic,
+            "traffic_level": "Moderate",
+            "route_coordinates": traffic_route_coordinates,
+            "nodes": len(traffic_route),
+        },
+    }
---
+++ frontend/dashboard.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import requests
+import folium
+
+from streamlit_folium import st_folium
+
+
+# m = folium.Map(location=[23.0225, 72.5714], zoom_start=12)
+# st_folium(m, width=700, height=500)
+
+st.title("Intelligent Route Optimization System")
+
+
+origin_lat = st.number_input("Origin Latitude", value=23.0225)
+
+origin_lon = st.number_input("Origin Longitude", value=72.5714)
+
+destination_lat = st.number_input("Destination Latitude", value=23.0400)
+
+destination_lon = st.number_input("Destination Longitude", value=72.5900)
+
+
+if st.button("Compute Route"):
+    try:
+        response = requests.get(
+            "http://127.0.0.1:8000/route",
+            params={
+                "origin_lat": origin_lat,
+                "origin_lon": origin_lon,
+                "destination_lat": destination_lat,
+                "destination_lon": destination_lon,
+            },
+        )
+
+        data = response.json()
+
+        shortest_route = data["shortest_route"]
+
+        traffic_route = data["traffic_aware_route"]
+
+        # shortest route coordinates
+        shortest_coordinates = []
+
+        for coord in shortest_route["route_coordinates"]:
+            lat = float(coord[0])
+
+            lon = float(coord[1])
+
+            shortest_coordinates.append([lat, lon])
+
+        # traffic-aware coordinates
+        traffic_coordinates = []
+
+        for coord in traffic_route["route_coordinates"]:
+            lat = float(coord[0])
+
+            lon = float(coord[1])
+
+            traffic_coordinates.append([lat, lon])
+
+        # create map
+        m = folium.Map(location=shortest_coordinates[0], zoom_start=13)
+
+        # SHORTEST ROUTE
+        # blue
+
+        folium.PolyLine(
+            locations=shortest_coordinates,
+            weight=5,
+            color="blue",
+            tooltip=("Shortest Distance Route"),
+        ).add_to(m)
+
+        # TRAFFIC-AWARE ROUTE
+        # red
+
+        folium.PolyLine(
+            locations=traffic_coordinates,
+            weight=5,
+            color="red",
+            tooltip=("Traffic-Aware Route"),
+        ).add_to(m)
+
+        # origin marker
+        folium.Marker(
+            shortest_coordinates[0], popup="Origin", icon=folium.Icon(color="green")
+        ).add_to(m)
+
+        # destination marker
+        folium.Marker(
+            shortest_coordinates[-1],
+            popup="Destination",
+            icon=folium.Icon(color="darkred"),
+        ).add_to(m)
+
+        # render map
+        st_folium(m, width=700, height=500, returned_objects=[])
+
+    except Exception as e:
+        st.error(str(e))
